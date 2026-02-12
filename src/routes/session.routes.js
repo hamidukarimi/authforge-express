0d8e6037dc0edecabfef01141d787cf7238b57f5
Hamid Karimi <hameedukarimi@gmail.com>
2026-02-13 00:58:26 +0430

    feat: add IP-based login rate limiter (5 attempts per 10 min)

--- src/routes/session.routes.js
+++ src/routes/session.routes.js
@@ -1,8 +1,9 @@
 import express from "express";
 import * as sessionController from "../controllers/session.controller.js";
+import { loginLimiter } from "../middlewares/rateLimit.middleware.js";
 
 const router = express.Router();
 
-router.post("/", sessionController.create);
+router.post("/", loginLimiter, sessionController.create);
 
 export default router;
